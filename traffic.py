56421c9f8fd0df3b09f9ba6405e481654d239690
Reiushka <159531039+Reiushka@users.noreply.github.com>
2025-02-21 09:11:54 +0200

    Add files via upload

---
+++ traffic.py
@@ -0,0 +1,43 @@
+from scapy.all import sniff, wrpcap, conf
+import os
+
+# Функція обробки перехоплених пакетів
+def packet_callback(packet):
+    # Визначення типу протоколу
+    if packet.haslayer("TCP"):
+        folder = "TCP"
+    elif packet.haslayer("UDP"):
+        folder = "UDP"
+    elif packet.haslayer("ICMP"):
+        folder = "ICMP"
+    else:
+        folder = "Other"
+    
+    # Перевірка наявності шифрування
+    if packet.haslayer("TLS") or packet.haslayer("SSL"):
+        encryption = "Encrypted"
+    else:
+        encryption = "Unencrypted"
+    
+    save_packet(packet, folder, encryption)
+
+# Функція збереження пакетів у відповідний каталог
+def save_packet(packet, folder, encryption):
+    path = f"traffic/{folder}/{encryption}"
+    os.makedirs(path, exist_ok=True)  # Створення директорії, якщо її немає
+    filename = f"{path}/packets.pcap"
+    
+    # Збереження пакета в файл та додавання до існуючого
+    wrpcap(filename, packet, append=True)
+    
+    # Виведення повідомлення в консоль
+    print(f"Saved packet to {filename} - {folder} / {encryption}")
+
+# Визначення мережевого інтерфейсу для перехоплення
+default_iface = "enp2s0"  # Вказано інтерфейс enp2s0
+
+print(f"Sniffing network traffic on interface: {default_iface}")
+
+# Запуск прослуховування мережевого трафіку
+sniff(iface=default_iface, prn=packet_callback, store=False)
+
